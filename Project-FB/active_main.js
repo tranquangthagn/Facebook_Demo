79ccee81107a79d986b3c0b03ccff88920159eae
tranquangthagn <83894128+tranquangthagn@users.noreply.github.com>
2022-04-16 15:55:59 +0700

    update login

--- Project-FB/active_main.js
+++ Project-FB/active_main.js
@@ -5,3 +5,41 @@ function effectshow() {
     detailEffect.style.display="none"
     lengtEffect.classList.remove("main-effect_effect-list-1")
 }
+// -----------------meet-----------
+var moveMeetNext=document.querySelector(".main-effect_PostStatus-meetZomm-next")
+var moveMeetBack=document.querySelector(".main-effect_PostStatus-meetZomm-back")
+var moveMeetUser=document.querySelector(".main-effect_PostStatus-meetZomm-slice")
+var plus=0;
+var getliuser=document.querySelectorAll(".PostStatus-createMeet-user")
+var numberGetliUser=getliuser.length
+var witdhmoveMeetUser=numberGetliUser*46 +211
+function tranthang(a) {
+    
+}
+
+moveMeetNext.onclick=()=>{
+plus+=590
+if (plus>(witdhmoveMeetUser-590)) {
+    moveMeetNext.style.display="none"   
+}
+moveMeetBack.style.display="block"   
+ moveMeetUser.style.transform=`translateX(${-plus}px)`   
+
+}
+
+
+moveMeetBack.onclick=()=>{
+   plus-=590;
+   if (plus<590) {
+    moveMeetBack.style.display="none"      
+}
+ moveMeetNext.style.display="block"   
+ moveMeetUser.style.transform=`translateX(${-plus}px)`
+ console.log(plus);   
+ }
+  
+//  ------------
+
+function signout(){
+    window.location.replace("./Page_Login.html")
+   } 
---
+++ UI Face/validator__login.js
@@ -0,0 +1,164 @@
+var accoutAPI="http://localhost:3000/account"
+  
+var createAcout=document.querySelector(".register")    
+createAcout.onclick=()=>{
+ document.querySelector(".modal").style.display="block"
+  
+} 
+
+var closeSignIn=document.querySelector(".auth-form_header-right")
+closeSignIn.onclick=()=>{
+  document.querySelector(".modal").style.display="none"    
+
+}
+function activeWeb(){
+       GetJsonOrigin()
+      handelLinks()
+    }
+    activeWeb()
+    // // lay data tu json
+    function GetJsonOrigin(callBack){
+      fetch(accoutAPI)
+    .then(function(JsonLinks){
+      return JsonLinks.json()
+    })
+    .then(callBack)
+    }
+  
+    // kiem tra email
+  
+    var TagetEmail= document.querySelector('.auth-form_password')
+    var getFormSign=document.querySelector(".auth-form_wrap--form")
+   
+    getFormSign.onclick= function(e){
+      if (e.target==TagetEmail) {  
+        var tranthang= document.querySelector('input[placeholder="Enter email address"]').value
+        fetch(accoutAPI)
+        .then(function(JsonLinks){
+          return JsonLinks.json()
+        })
+        .then(listAcout=>{
+
+            const getAsEmail= listAcout.some(element => {
+                return element.email===tranthang
+
+            });
+            if (getAsEmail) {
+           alert("email đa được dùng")
+           document.querySelector('input[placeholder="Enter email address"]').value=" "
+           }
+           else{   
+           return 1;
+               }
+        })
+      }
+    }
+     // kiem tra accout
+    //  var passAccountCheck=document.querySelector('input[name="password"]')
+    //  passAccountCheck.onclick=()=>{
+      
+
+    //  }
+    
+     var btnLogin=document.querySelector(".form-submit")
+     btnLogin.onclick=()=>{ 
+       
+      var emailAccount=document.querySelector('input[name="email"]').value
+      var passAccount=document.querySelector('input[name="password"]').value
+       if (emailAccount===""&& passAccount==="") {
+         console.log("mess");
+       }else  {
+
+        fetch(accoutAPI)
+        .then(valueJson=>{
+          return valueJson.json()
+        })
+        .then(json=>{
+          const getAsEmail= json.find(element => {
+            return element.email===emailAccount
+
+        })
+        const getAsEmail1= json.some(element => {
+          return element.passwork===passAccount
+
+      })
+       
+        if (getAsEmail1===false) {
+          alert("tài khoản không tồn tại")
+
+        }else{
+       console.log("fdsfads");
+       window.location.replace("home.html")
+        }
+        })
+       }
+
+     }
+
+    // // Khoi tao danh sach
+    function creatElement(data,calbacklink){
+    const respone={
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data)
+    }
+    fetch(accoutAPI,respone)
+    .then(function(newValue){
+      return newValue.json()
+    })
+    .then(calbacklink)
+    }
+    var html="" 
+    function handelLinks(){
+      var handelbtn=document.querySelector(".submit-btn")
+      handelbtn.onclick=function(){
+
+        var FirstName= document.querySelector('input[placeholder="First name"]').value;
+        var SubName= document.querySelector('input[placeholder="Surname"]').value
+        var email= document.querySelector('input[placeholder="Enter email address"]').value
+        var passwork= document.querySelector('.auth-form_password').value
+        var day= document.querySelector('select[name="birthday-day"] option').value
+        var month= document.querySelector('select[name="birthday-month"] option').value
+        var year= document.querySelector('select[name="birthday-year"] option').value
+        var gender= document.querySelector('input[type="radio"]').value
+        var genderCheck= document.querySelector('input[type="radio"]').checked
+
+        // ------------fdds
+     
+        if (FirstName ==html || SubName ==html || email==html || passwork==html ||genderCheck==false) {
+          console.log("messs");
+          }
+          else{    
+              var formData={
+                     FirstName:FirstName ,
+                     SubName: SubName,
+                     email: email,
+                     passwork: passwork,
+                     day: day,
+                     month: month,
+                     year: year,
+                     gender: gender,
+                }   
+             creatElement(formData,function(){
+                 GetJsonOrigin()
+               }) 
+               window.location.replace("./index.html")  
+               document.querySelector(".modal").style.display="none"    
+                   
+               }
+                }
+                }
+          
+
+
+             
+             
+           
+            
+            
+   
+        
+                     
